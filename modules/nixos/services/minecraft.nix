751dc30e0de979d615530a105b9e63856ccb5be2
hermlon <hermlon@yuustan.space>
2025-12-01 01:21:56 +0100

    Added Geyser plugin

--- modules/nixos/services/minecraft.nix
+++ modules/nixos/services/minecraft.nix
@@ -19,7 +19,13 @@ in {
       enable = true;
       eula = true;
 
-      servers = {
+      servers = let
+        geyser = pkgs.fetchurl {
+          url = "https://download.geysermc.org/v2/projects/geyser/versions/2.9.1/builds/999/downloads/spigot";
+          hash = "sha256-5f21qdfY2SZUDqknf1bGU846GGoSkzjDELmgsrvr2Rs=";
+          name = "geyser.jar";
+        };
+      in {
         server-1 = {
           enable = true;
           package = pkgs.paperServers.paper-1_21_8;
@@ -30,8 +36,20 @@ in {
             motd = "Minecraft Server 1";
             difficulty = "normal";
           };
+          operators = {
+            hermlon = "18b452dc-ec5d-4c96-bbed-7f13afc079aa";
+            Altron_tcs = "b8840711-6c0d-41ea-bc4a-07cb665f3744";
+            Mattix_MC_ = "14b65485-19f4-4dc5-b56e-861d24a70a94";
+          };
+          symlinks = {
+            "plugins/geysermc.jar" = geyser;
+          };
         };
       };
+    };
+
+    networking.firewall = {
+      allowedUDPPorts = [19132];
     };
   };
 }
